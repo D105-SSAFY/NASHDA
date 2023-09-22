81d2ce76857caed831e362822e92b184e7cbc12d
hyerdd <hyerdd@naver.com>
2023-09-22 14:53:06 +0900

    Feat : 문제 서버 MongoDB 컬렉션 데이터 수정 및 저장 기능 추가

--- backend/nashda-problem/src/main/java/com/ssafy/nashda/practice/service/PracticePronService.java
+++ backend/nashda-problem/src/main/java/com/ssafy/nashda/practice/service/PracticePronService.java
@@ -1,5 +1,6 @@
 package com.ssafy.nashda.practice.service;
 
+import com.ssafy.nashda.practice.dto.PronSaveRequestDto;
 import com.ssafy.nashda.practice.entity.PronComplexSet;
 import com.ssafy.nashda.practice.entity.PronPhaseSet;
 import com.ssafy.nashda.practice.entity.PronSimpleSet;
@@ -15,26 +16,25 @@ import org.springframework.web.multipart.MultipartFile;
 public interface PracticePronService {
     // 단어 연습 문제 불러오기 및 저장 기능
     PronWordSet getPronWordSets(int index) throws Exception;
-    PronWordSet savePronWordSet() throws Exception;
+    PronWordSet savePronWordSet(PronSaveRequestDto pronSaveRequestDto) throws Exception;
 
     // 구 연습 문제 불러오기 및 저장 기능
     PronPhaseSet getPronPhaseSets(int index) throws Exception;
-    PronPhaseSet savePronPhaseSet() throws Exception;
+    PronPhaseSet savePronPhaseSet(PronSaveRequestDto pronSaveRequestDto) throws Exception;
 
 
     // 절 연습 문제 불러오기 및 저장 기능
     PronSimpleSet getPronSimpleSets(int index) throws Exception;
-    PronSimpleSet savePronSimpleSet() throws Exception;
+    PronSimpleSet savePronSimpleSet(PronSaveRequestDto pronSaveRequestDto) throws Exception;
 
 
     // 복합절 연습 문제 불러오기 및 저장 기능
     PronComplexSet getPronComplexSets(int index) throws Exception;
-    PronComplexSet savePronComplexSet() throws Exception;
+    PronComplexSet savePronComplexSet(PronSaveRequestDto pronSaveRequestDto) throws Exception;
 
     // 문제 개수 반환
     // 어떤 문제 불러올지를 인자로 받아옴
     long getPronSetNum(String seqName) throws Exception;
 
 
-    String getSTT(MultipartFile multipartFile, long index, String type);
 }
