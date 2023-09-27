a4f065f21b5fbc013268a3b265d2a14c80558a38
우주연 <dnwndus01@naver.com>
2023-09-27 10:04:28 +0900

    Feat : 회원가입, 비밀번호 찾기 api 연결중
    css, alert 대체만 하면됨.

--- frontend/src/pages/userpage/signuppage/style.js
+++ frontend/src/pages/userpage/signuppage/style.js
@@ -44,7 +44,10 @@ export const StyledForm = styled.form`
 `;
 
 export const StyledText = styled.p`
-    font-size: 20px;
+    width: 100%;
+    text-align: left;
+    margin: 5px 0 0 5px;
+    font-size: 16px;
     color: ${(props) =>
         props.colorEmail === 2 || props.colorEmail === 5 || props.colorPassword === 1 || props.colorPassword === 3 || props.colorNickname === 2
             ? "blue"
