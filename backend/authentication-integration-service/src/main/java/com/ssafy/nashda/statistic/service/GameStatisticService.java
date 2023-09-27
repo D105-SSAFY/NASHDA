7a7078ca303896363f6f15ac64ba4e0503cc1f7e
silver <hara0822@naver.com>
2023-09-27 12:51:45 +0900

    feat : statistic-game-back 완성

--- backend/authentication-integration-service/src/main/java/com/ssafy/nashda/statistic/service/GameStatisticService.java
+++ backend/authentication-integration-service/src/main/java/com/ssafy/nashda/statistic/service/GameStatisticService.java
@@ -2,6 +2,7 @@ package com.ssafy.nashda.statistic.service;
 
 import com.ssafy.nashda.member.entity.Member;
 import com.ssafy.nashda.statistic.dto.response.BlankStatisticResDto;
+import com.ssafy.nashda.statistic.dto.response.GameStatisticResDto;
 import com.ssafy.nashda.statistic.dto.response.SpeedStatisticResDto;
 import com.ssafy.nashda.statistic.entity.GameStatistic;
 import com.ssafy.nashda.week.entity.Week;
@@ -11,8 +12,8 @@ import java.util.Optional;
 
 public interface GameStatisticService {
     
-    List<GameStatistic> getGameStatistic(Member member);    //게임통계5주치 불러오기
-    Optional<GameStatistic> getGameStatisticByWeek(Member member, Week week);   //오늘 주에 해당하는 게임 통계 불러오기
+    List<GameStatisticResDto> getGameStatistic(Member member);    //게임통계5주치 불러오기
+    Optional<GameStatisticResDto> getGameStatisticByWeek(Member member, Week week);   //오늘 주에 해당하는 게임 통계 불러오기
     BlankStatisticResDto getBlankStatistic(Member member, Week week);   //블랭크 게임 통계 불러오기
     SpeedStatisticResDto getSpeedStatistic(Member member, Week week);   //스피드 게임 통계 불러오기
 }
