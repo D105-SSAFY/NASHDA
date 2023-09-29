da557140073801f3226ee411e8dfeda77df4b756
안세혁 <he2kape@gmail.com>
2023-09-29 22:33:03 +0900

    Feat : 마이페이지 하는 중이여씀

--- frontend/src/components/mypage/user/archievement/Archievement.jsx
+++ frontend/src/components/mypage/user/archievement/Archievement.jsx
@@ -5,14 +5,14 @@ import { MoreButton } from "components/mypage/user/style";
 import Emoji from "assets/image/emoji/emoji0.png";
 import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOutlined";
 
-export default function Archievement({ getMore }) {
+export default function Archievement({ setMore }) {
     return (
         <>
             <a.ArchieveBox>
                 <a.ArchieveEmoji src={Emoji} />
                 <a.ArchieveText>6+</a.ArchieveText>
             </a.ArchieveBox>
-            <MoreButton onClick={() => getMore(3)}>
+            <MoreButton onClick={() => setMore(3)}>
                 달성
                 <ArrowCircleRightOutlinedIcon />
             </MoreButton>
