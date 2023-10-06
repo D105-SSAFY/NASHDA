5037e77e2fcf4b928117ecaec3c5bc422e4b2389
sdh20282 <sdh20282@gmail.com>
2023-10-06 10:19:06 +0900

    Refactor : 로그인/회원가입 페이지에서 사용하는 모달 이미지, 폰트 수정

--- frontend/src/components/modals/signupmodal/SignupModal.jsx
+++ frontend/src/components/modals/signupmodal/SignupModal.jsx
@@ -1,7 +1,9 @@
 import * as s from "./style";
+
 import FilledButton from "components/buttons/filledbutton/FilledButton";
-import SuccessIcon from "assets/image/icons8-승인-144.png";
-import FailIcon from "assets/image/icons8-오류-96.png";
+
+import SuccessIcon from "assets/image/accept.png";
+import FailIcon from "assets/image/warning.png";
 
 export default function SignupModal({ props: { text, visible, callback } }) {
     return (
@@ -13,7 +15,7 @@ export default function SignupModal({ props: { text, visible, callback } }) {
                 {visible === "success" ? <s.SuccessImg src={SuccessIcon}></s.SuccessImg> : <s.SuccessImg src={FailIcon}></s.SuccessImg>}
                 <s.Pron>{text}</s.Pron>
                 <s.ButtonWrapper>
-                    <FilledButton props={{ background: "rgba(68, 71, 90, 0.7)", color: "#ffffff", hovercolor: "#44475A", callback }}>
+                    <FilledButton props={{ background: "rgb(68, 71, 90, 0.7)", color: "#ffffff", hovercolor: "#44475A", callback }}>
                         <span>확인</span>
                     </FilledButton>
                 </s.ButtonWrapper>
